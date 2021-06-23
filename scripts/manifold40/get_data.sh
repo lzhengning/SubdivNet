687defa6c893d2af26dbba4d8f6f4b2bde8fbfe9
Zheng-Ning Liu <lzhengning@gmail.com>
2021-06-23 17:12:54 +0800

    Update Manifold40-MAPS dataset downloading scripts

--- scripts/manifold40/get_data.sh
+++ scripts/manifold40/get_data.sh
@@ -3,7 +3,7 @@
 DATADIR=$(dirname $0)/'../../data'
 
 mkdir -p $DATADIR && cd $DATADIR
-wget --content-disposition https://cloud.tsinghua.edu.cn/f/2a292c598af94265a0b8/?dl=1
+wget --content-disposition https://cloud.tsinghua.edu.cn/f/af5c682587cc4f9da9b8/?dl=1
 echo "downloaded the data and putting it in: " $DATADIR
 echo "unzipping"
-unzip -q Manifold40.zip && rm Manifold40.zip
+unzip -q Manifold40-MAPS-96-3.zip && rm Manifold40-MAPS-96-3.zip
